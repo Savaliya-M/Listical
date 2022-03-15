f52ec828ae3ba0caa24f60d516f62331d9a8a87c
Shubham0-n <5048shubham2028@gmail.com>
2022-03-15 09:06:49 +0530

    project details updated

--- src/routes/projects/Projectdetail.js
+++ src/routes/projects/Projectdetail.js
@@ -43,41 +43,93 @@ const Projectdetail = () => {
 
   return (
     <>
-      <div>
-        <div>
-          <h1>project basic Detail</h1>
-          <h2>{project.projectTitle}</h2>
-          <h3>{project.clientName}</h3>
-          <h4>{project.timeLine}</h4>
-        </div>
-        <div>
-          <h1>project Description</h1>
-        </div>
-        <div>
-          <h1>Team Member</h1>
-          {localStorage.getItem("Type") === "Manager" ? (
-            <button
-              onClick={() =>
-                navigate(`/layout/project/projectdetail/employeeadd/${id}`)
-              }
-            >
-              +
-            </button>
-          ) : (
-            ""
-          )}
 
-          {Object.values(team).map((id) => (
-            <div key={id.uuid}>
-              <h1> Name:{id.name}</h1> <br />
-              <h3> mono:{id.mono} </h3>
-              <br />
-              <h3> post:{id.post} </h3>
-              <br />
+      <div className={prodetail.all}>
+        <div className={prodetail.alldetails}>
+          <div className={prodetail.Progress}>
+            <h1>Progress Bar</h1>
+          </div>
+          <div className={prodetail.rightside}>
+
+            <div className={prodetail.graphs}>
+
+              <img src={require("@photos/LineGraphs.jpg")} alt="" />
             </div>
-          ))}
+
+            <div className={prodetail.prodetails}>
+              <div className={prodetail.client}>
+                <div className={prodetail.heading}>
+                  <h2>Project Basic Detail</h2>
+                </div>
+                <h5>Poject Name</h5>
+                <h2>{project.projectTitle}</h2>
+                <h5>Owner Name</h5>
+                <h3>{project.clientName}</h3>
+                <h5>Dead Line</h5>
+                <h4>{project.timeLine}</h4>
+              </div>
+
+              <div className={prodetail.aboutproject}>
+                <div className={prodetail.heading}>
+                  <h2>Project Description</h2>
+                </div>
+                <div className={prodetail.para}>
+                  <div>
+                    <h5>Frontend Technology</h5>
+                    <h3> Html - CSS - JavaScript</h3>
+                  </div>
+                  <div>
+                    <h5>Backend Technology</h5>
+                    <h3> Programing lan. - PHP</h3>
+                    <h3>Framwork - Laravel</h3>
+                    <h3>Web Servers - Apache</h3>
+                    <h3>Database - Mysql</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className={prodetail.aboutteam}>
+            <div className={prodetail.heading}>
+              <h2>Team Member</h2>
+              <div className={prodetail.addbtn}>
+                {localStorage.getItem("Type") === "Manager" ? (
+                  <button
+                    onClick={() =>
+                      navigate(`/layout/project/projectdetail/employeeadd/${id}`)
+                    }
+                  >
+                    +
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+            <div className={prodetail.team}>
+              {Object.values(team).map((id) => (
+                <div key={id.uuid} className={prodetail.teammember}>
+                  <div className={prodetail.names}>
+                    <h3>Name</h3>
+                    <h2>{id.name}</h2>
+                  </div>
+                  <div className={prodetail.mono}>
+                    <h3>Mobile No.</h3>
+                    <h2>{id.mono} </h2>
+                  </div>
+                  <div className={prodetail.post}>
+                    <h3>Post</h3>
+                    <h2>{id.post} </h2>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
+
     </>
   );
 };
