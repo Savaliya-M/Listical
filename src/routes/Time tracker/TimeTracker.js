2ddb303bf48cba88fd26059a839837635fbe5d63
Savaliya-M <masavaliya183@gmail.com>
2022-04-07 10:57:51 +0530

    salary page completed

--- src/routes/Time tracker/TimeTracker.js
+++ src/routes/Time tracker/TimeTracker.js
@@ -5,9 +5,9 @@ import { v4 as uuidv4 } from "uuid";
 const TimeTracker = () => {
   const [trackRecord, setTrackRecord] = useState({
     tag: "",
-    startTime: Date(),
+    startTime: "",
     endTime: "",
-    uid: uuidv4(),
+    uid: "",
     flag: false,
     workingTime: "",
   });
@@ -16,12 +16,9 @@ const TimeTracker = () => {
   const [temptrackRecord, setTemptrackRecord] = useState({});
   const [startEndBtn, setstartEndBtn] = useState(false);
   const [id, setid] = useState("");
-  // const [timer, setTimer] = useState({
-  //   ho: 0,
-  //   mi: 0,
-  //   se: 0,
-  // });
+  const [myTimerIntervalID, setMyTimerIntervalID] = useState(null);
 
+  // *********************************************************************************************************************************
   useEffect(() => {
     appRef
       .child(`TimeTracker/${uid}`)
@@ -34,21 +31,14 @@ const TimeTracker = () => {
   }, []);
 
   useEffect(() => {
-    let flag = true;
-    if (flag) {
-      appRef.child(`TimeTracker/${uid}`).on("value", (snap) => {
-        setOldData(snap.val());
-        flag = false;
-      });
-    }
-  }, []);
-
-  useEffect(() => {
     if (id && id.length) {
       appRef.child(`TimeTracker/${uid}/${id}`).on("value", (snap) => {
         if (snap.val() && Object.values(snap.val()).length) {
           if (snap.val().flag === false) {
-            setstartEndBtn(!startEndBtn);
+            console.log("gfghfjfjh");
+            if (snap.val().startTime) {
+              setstartEndBtn(true);
+            }
           } else {
             return "";
           }
@@ -60,62 +50,102 @@ const TimeTracker = () => {
     }
   }, [id, uid]);
 
-  let d = new Date(temptrackRecord.startTime).getTime();
-
-  setInterval(() => {
-    let n = new Date().getTime();
-    let h = Math.floor((n - d) / 1000 / 60 / 60);
-    let m = Math.floor(((n - d) / 1000 / 60 / 60 - h) * 60);
-    let s = Math.floor((((n - d) / 1000 / 60 / 60 - h) * 60 - m) * 60);
+  useEffect(() => {
+    console.log("IN state chnage", startEndBtn);
     if (startEndBtn) {
-      if (
-        (Number(h) || Number(h) === 0) &&
-        (Number(m) || Number(m) === 0) &&
-        (Number(s) || Number(s) === 0)
-      ) {
-        let timeTag = document.getElementById("timer");
-        timeTag.innerHTML = h + " : " + m + " : " + s;
+      startMyInterval();
+    } else {
+      console.log("myTimerIntervalID", myTimerIntervalID);
+      if (myTimerIntervalID) {
+        clearInterval(myTimerIntervalID);
+        setMyTimerIntervalID(null);
       }
     }
-  }, 1000);
+  }, [startEndBtn]);
 
-  const recordChange = (e) => {
-    setTrackRecord({ ...trackRecord, [e.target.name]: e.target.value });
-  };
+  useEffect(() => {
+    let flag = true;
+    if (flag) {
+      appRef.child(`TimeTracker/${uid}`).on("value", (snap) => {
+        setOldData(snap.val());
+        flag = false;
+      });
+    }
+  }, []);
+
+  // *********************************************************************************************************************************
 
   const recordstartClick = () => {
-    appRef.child(`TimeTracker/${uid}`).push(trackRecord);
-    setTrackRecord({
-      tag: "",
+    let sendData = {
+      tag: trackRecord.tag,
       startTime: Date(),
       endTime: "",
       uid: uuidv4(),
       flag: false,
       workingTime: "",
-    });
+    };
+    appRef.child(`TimeTracker/${uid}`).push(sendData);
+    console.log(sendData);
+    setTrackRecord(sendData);
+    setstartEndBtn(!startEndBtn);
   };
+
+  const startMyInterval = () => {
+    let d = new Date(trackRecord.startTime).getTime();
+    let intervalID = setInterval(() => {
+      console.log("IN interval");
+      let n = new Date().getTime();
+      let h = Math.floor((n - d) / 1000 / 60 / 60);
+      let m = Math.floor(((n - d) / 1000 / 60 / 60 - h) * 60);
+      let s = Math.floor((((n - d) / 1000 / 60 / 60 - h) * 60 - m) * 60);
+      if (startEndBtn) {
+        if (
+          (Number(h) || Number(h) === 0) &&
+          (Number(m) || Number(m) === 0) &&
+          (Number(s) || Number(s) === 0)
+        ) {
+          let timeTag = document.getElementById("timer");
+          timeTag.innerHTML = h + " : " + m + " : " + s;
+        }
+      }
+    }, 1000);
+    setMyTimerIntervalID(intervalID);
+    console.log("myTimerIntervalIDdsfhjdksfkjs", myTimerIntervalID);
+  };
+
   const recordendClick = () => {
     let t = new Date() - new Date(temptrackRecord.startTime);
-    temptrackRecord.flag = true;
-    temptrackRecord.endTime = Date();
-    temptrackRecord.workingTime = t;
+    trackRecord.flag = true;
+    trackRecord.endTime = Date();
+    trackRecord.workingTime = t;
+    appRef.child(`TimeTracker/${uid}/${id}`).set(trackRecord);
+    console.log(trackRecord);
+    const abc = !startEndBtn;
+    console.log("end", abc);
+    setstartEndBtn(abc);
+  };
 
-    appRef.child(`TimeTracker/${uid}/${id}`).set(temptrackRecord);
-    setstartEndBtn(!startEndBtn);
+  const recordChange = (e) => {
+    setTrackRecord({ ...trackRecord, [e.target.name]: e.target.value });
   };
 
   const getDateTime = (time) => {
     return new Date(time).toLocaleTimeString();
   };
+
+  const getDate = (timeDate) => {
+    return new Date(timeDate).toLocaleDateString();
+  };
+
   const gettime = (time) => {
     let hours = Math.floor(time / 1000 / 60 / 60);
     let minutes = Math.floor((time / 1000 / 60 / 60 - hours) * 60);
     let seconds = Math.floor(
       ((time / 1000 / 60 / 60 - hours) * 60 - minutes) * 60
     );
-
     return hours + " : " + minutes + " : " + seconds;
   };
+  // *********************************************************************************************************************************
 
   return (
     <>
@@ -130,31 +160,29 @@ const TimeTracker = () => {
             onChange={recordChange}
           />
 
-          {startEndBtn === false ? (
+          {!startEndBtn ? (
             <button onClick={recordstartClick}>Start</button>
           ) : (
             <button onClick={recordendClick}>End</button>
           )}
         </div>
-        {startEndBtn === true ? (
+        {startEndBtn ? (
           <div>
-            {/* {timerClock()} */}
             <p id="timer"></p>
-            {/* Time : {timer.ho} : {timer.mi} : {timer.se} */}
           </div>
         ) : (
           <></>
         )}
         <div>
           {oldData && oldData.length !== 0
-            ? // ? console.log(oldData)
-              Object.keys(oldData).map((id) => {
+            ? Object.keys(oldData).map((id) => {
                 return (
                   <div key={id}>
                     <p>
                       ============================================================
                     </p>
                     <p>{oldData[id].tag}</p>
+                    <p>{getDate(oldData[id].startTime)}</p>
                     <p>{getDateTime(oldData[id].startTime)}</p>
                     <p>
                       {oldData[id].endTime && oldData[id].endTime.length !== 0
--- src/routes/user/userinfo/Userinfo.js
+++ src/routes/user/userinfo/Userinfo.js
@@ -3,6 +3,8 @@ import useinfo from "./userinfo.module.scss";
 import { Link, useNavigate } from "react-router-dom";
 import appRef from "../../../firebase";
 import { useParams } from "react-router-dom";
+import Salary from "./Salary";
+import { v4 as uuidv4 } from "uuid";
 
 const Userinfo = () => {
   let navigate = useNavigate();
@@ -12,6 +14,8 @@ const Userinfo = () => {
   const { id } = useParams();
   const [projects, setProjects] = useState({});
   const [userProjects, setUserProjects] = useState([]);
+  const [salaryPage, setSalaryPage] = useState(false);
+
   useEffect(() => {
     appRef.child(`Users/${id}`).on("value", (snapshot) => {
       setUser(snapshot.val());
@@ -54,211 +58,244 @@ const Userinfo = () => {
     }
   }, [projects, user]);
 
+  const openSalary = () => {
+    setSalaryPage(!salaryPage);
+  };
   return (
     <>
-      <div className={useinfo.mainuserinfo}>
-        <div className={useinfo.secuserinfo}>
-          <div className={useinfo.closebtn}>
-            <button className={useinfo.closebtn1}
-              onClick={() => {
-                navigate(-1);
-              }}
-            >
-              X
-            </button>
-            {user.uuid === localStorage.getItem("uuid") ? (
-              <button className={useinfo.closebtn2}
-              onClick={() => navigate(`/layout/user/editdetail/${id}`)}>
-              <img src={require("@photos/edit(1).png")} alt="edit" />
+      {salaryPage ? (
+        <Salary closeSalary={openSalary} id={user.uuid} usalary={user.salary} />
+      ) : (
+        <div className={useinfo.mainuserinfo}>
+          <div className={useinfo.secuserinfo}>
+            <div className={useinfo.closebtn}>
+              <button
+                className={useinfo.closebtn1}
+                onClick={() => {
+                  navigate(-1);
+                }}
+              >
+                X
               </button>
-            ) : (
-              <></>
-            )}
-          </div>
-          <div className={useinfo.thirduserinfo}>
-            <div className={useinfo.basicdetail}>
-              <div className={useinfo.card}>
-                <div className={useinfo.personimage}>
-                  <img src={require("@photos/man.png")} alt="person" />
-                </div>
-                <div className={useinfo.usercard}>
-                  <div className={useinfo.cardinfo}>
-                    <h1>{user.name}</h1>
-                    <p>{`${user.position}`}</p>
-                    <div className={useinfo.cardinfo}>
-                      <img src={require("@photos/Wcoding.png")} alt="skill" />
-                      {user.role}
-                    </div>
-                    <div className={useinfo.cardinfo}>
-                      <img src={require("@photos/Wcall.png")} alt="skill" />
-                      +91 {user.mono}
-                    </div>
-                    <div className={useinfo.cardinfo}>
-                      <img src={require("@photos/Wemail.png")} alt="skill" />
-                      {user.email}
-                    </div>
+              {user.uuid === localStorage.getItem("uuid") ? (
+                <button
+                  className={useinfo.closebtn2}
+                  onClick={() => navigate(`/layout/user/editdetail/${id}`)}
+                >
+                  <img src={require("@photos/edit(1).png")} alt="edit" />
+                </button>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div className={useinfo.thirduserinfo}>
+              <div className={useinfo.basicdetail}>
+                <div className={useinfo.card}>
+                  <div className={useinfo.personimage}>
+                    <img src={require("@photos/man.png")} alt="person" />
                   </div>
-                  <div onClick={useinfo.open} className={useinfo.userbtn}>
-                    <div className={useinfo.btnpersonal}>
-                      <button className={useinfo.cardbtn}> <a href="#personalinfo"><b>Information</b></a> </button>
-                    </div>
-                    <div className={useinfo.qualification}>
-                    <button className={useinfo.cardbtn}> <a href="#QualificationSkill"><b>Qualification & Skill</b></a> </button>
-                    </div>
-                    <div className={useinfo.work}>
-                      <button className={useinfo.cardbtn}> <a href="#experiances"><b>Experiance</b></a> </button>
-                    </div>
-                    <div className={useinfo.Projects}>
-                    <button className={useinfo.cardbtn}> <a href="#Projects"><b>Projects</b></a> </button>
-                    </div>
-                    {localStorage.getItem("Type") === "Admin" ||
-                    localStorage.getItem("Type") === "Manager" ||
-                    localStorage.getItem("uuid") === user.uuid ? (
-                      <div className={useinfo.btnsalary}>
-                        {/* <>Salary</a> */}
-                        <Link exact to={`/layout/user/salary/${user.uuid}`}>
-                          Salary
-                        </Link>
-                        {/* <button>Salary</button> */}
+                  <div className={useinfo.usercard}>
+                    <div className={useinfo.cardinfo}>
+                      <h1>{user.name}</h1>
+                      <p>{`${user.position}`}</p>
+                      <div className={useinfo.cardinfo}>
+                        <img src={require("@photos/Wcoding.png")} alt="skill" />
+                        {user.role}
                       </div>
-                    ) : (
-                      <></>
-                    )}
+                      <div className={useinfo.cardinfo}>
+                        <img src={require("@photos/Wcall.png")} alt="skill" />
+                        +91 {user.mono}
+                      </div>
+                      <div className={useinfo.cardinfo}>
+                        <img src={require("@photos/Wemail.png")} alt="skill" />
+                        {user.email}
+                      </div>
+                    </div>
+                    <div onClick={useinfo.open} className={useinfo.userbtn}>
+                      <div className={useinfo.btnpersonal}>
+                        <button className={useinfo.cardbtn}>
+                          {" "}
+                          <a href="#personalinfo">
+                            <b>Information</b>
+                          </a>{" "}
+                        </button>
+                      </div>
+                      <div className={useinfo.qualification}>
+                        <button className={useinfo.cardbtn}>
+                          {" "}
+                          <a href="#QualificationSkill">
+                            <b>Qualification & Skill</b>
+                          </a>{" "}
+                        </button>
+                      </div>
+                      <div className={useinfo.work}>
+                        <button className={useinfo.cardbtn}>
+                          {" "}
+                          <a href="#experiances">
+                            <b>Experiance</b>
+                          </a>{" "}
+                        </button>
+                      </div>
+                      <div className={useinfo.Projects}>
+                        <button className={useinfo.cardbtn}>
+                          {" "}
+                          <a href="#Projects">
+                            <b>Projects</b>
+                          </a>{" "}
+                        </button>
+                      </div>
+                      {localStorage.getItem("Type") === "Admin" ||
+                      localStorage.getItem("Type") === "Manager" ||
+                      localStorage.getItem("uuid") === user.uuid ? (
+                        <div className={useinfo.btnsalary}>
+                          {/* <>Salary</a> */}
+                          {/* <Link exact to={`/layout/user/salary/${user.uuid}`}>
+                            Salary
+                          </Link> */}
+                          <button onClick={openSalary}>Salary</button>
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className={useinfo.detailtext}>
-                <div className={useinfo.prinfo} id="personalinfo">
-                  <div className={useinfo.title1}>
-                    <h2>Personal Information</h2>
-                  </div>
-                  <div className={useinfo.info}>
-                    <div className={useinfo.infolist}>
-                      <h4>Name</h4>
-                      <h3>{user.name}</h3>
+                <div className={useinfo.detailtext}>
+                  <div className={useinfo.prinfo} id="personalinfo">
+                    <div className={useinfo.title1}>
+                      <h2>Personal Information</h2>
                     </div>
-                    {/* <div className={useinfo.infolist}>
+                    <div className={useinfo.info}>
+                      <div className={useinfo.infolist}>
+                        <h4>Name</h4>
+                        <h3>{user.name}</h3>
+                      </div>
+                      {/* <div className={useinfo.infolist}>
                       <h4>Post/Skill</h4>
                       <h3>{user.post}</h3>
                     </div> */}
-                    <div className={useinfo.infolist}>
-                      <h4>Mobile No.</h4>
-                      <h3>{user.mono}</h3>
-                    </div>
-                    <div className={useinfo.infolist}>
-                      <h4>Email ID</h4>
-                      <h3>{user.email}</h3>
-                    </div>
-                    {/* <div className={useinfo.infolist}>
+                      <div className={useinfo.infolist}>
+                        <h4>Mobile No.</h4>
+                        <h3>{user.mono}</h3>
+                      </div>
+                      <div className={useinfo.infolist}>
+                        <h4>Email ID</h4>
+                        <h3>{user.email}</h3>
+                      </div>
+                      {/* <div className={useinfo.infolist}>
                       <h4>Degree</h4>
                       <h3>{user.degree}</h3>
                     </div> */}
+                    </div>
                   </div>
-                </div>
 
-                <div id="QualificationSkill" className={useinfo.Qskill}>
-                  <div className={useinfo.title2}>
-                    <h2>Qualification & Skill</h2>
-                  </div>
-                  <div className={useinfo.info2}>
-                    <div className={useinfo.infolist2}>
-                      <h4>Degree</h4>
-                      <h3>{user.degree}</h3>
+                  <div id="QualificationSkill" className={useinfo.Qskill}>
+                    <div className={useinfo.title2}>
+                      <h2>Qualification & Skill</h2>
                     </div>
-                    <div className={useinfo.infolist2}>
-                      <h4>Collage</h4>
-                      <h3>{user.colname}</h3>
-                    </div>
-                    <div className={useinfo.infolist2}>
-                      <h4>Language Known</h4>
-                      <h3>
-                        {langknow.map((elem) => {
-                          if (elem !== "") {
-                            <li key={elem}>
-                              {elem}
-                              <br />
-                            </li>;
-                          }
-                          return elem;
-                        })}
-                      </h3>
-                    </div>
-                    <div className={useinfo.scrollname}>
-                      <h4>Skill</h4>
-                      <div id={useinfo.list} className={useinfo.infolist2}>
+                    <div className={useinfo.info2}>
+                      <div className={useinfo.infolist2}>
+                        <h4>Degree</h4>
+                        <h3>{user.degree}</h3>
+                      </div>
+                      <div className={useinfo.infolist2}>
+                        <h4>Collage</h4>
+                        <h3>{user.colname}</h3>
+                      </div>
+                      <div className={useinfo.infolist2}>
+                        <h4>Language Known</h4>
                         <h3>
-                          {skill.map((elem) => (
-                            <li key={elem}>{elem}</li>
-                          ))}
+                          {langknow.map((elem) => {
+                            if (elem !== "") {
+                              <li key={elem}>
+                                {elem}
+                                <br />
+                              </li>;
+                            }
+                            return elem;
+                          })}
                         </h3>
                       </div>
+                      <div className={useinfo.scrollname}>
+                        <h4>Skill</h4>
+                        <div id={useinfo.list} className={useinfo.infolist2}>
+                          <h3>
+                            {skill.map((elem) => (
+                              <li key={elem}>{elem}</li>
+                            ))}
+                          </h3>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div id="experiances" className={useinfo.experiances}>
-                  <div className={useinfo.title3}>
-                    <h2>WORK EXPERIENCE</h2>
-                  </div>
-                  <div className={useinfo.info3}>
-                    <div className={useinfo.infolist3}>
-                      <h4>Post</h4>
-                      <h3>{user.post} </h3>
+                  <div id="experiances" className={useinfo.experiances}>
+                    <div className={useinfo.title3}>
+                      <h2>WORK EXPERIENCE</h2>
                     </div>
-                    <div className={useinfo.infolist3}>
-                      <h4>Company</h4>
-                      <h3>{user.precompany}</h3>
-                    </div>
-                    <div className={useinfo.infolist3}>
-                      <h4>Duration</h4>
-                      <h3>{user.preworkduration}</h3>
-                    </div>
-                    <div className={useinfo.empwordsscroll}>
-                      <h4>Employee Words</h4>
-                      <div id={useinfo.empwords} className={useinfo.infolist3}>
-                        <h3>{user.otherdetail}</h3>
+                    <div className={useinfo.info3}>
+                      <div className={useinfo.infolist3}>
+                        <h4>Post</h4>
+                        <h3>{user.post} </h3>
+                      </div>
+                      <div className={useinfo.infolist3}>
+                        <h4>Company</h4>
+                        <h3>{user.precompany}</h3>
+                      </div>
+                      <div className={useinfo.infolist3}>
+                        <h4>Duration</h4>
+                        <h3>{user.preworkduration}</h3>
+                      </div>
+                      <div className={useinfo.empwordsscroll}>
+                        <h4>Employee Words</h4>
+                        <div
+                          id={useinfo.empwords}
+                          className={useinfo.infolist3}
+                        >
+                          <h3>{user.otherdetail}</h3>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div id="Projects" className={useinfo.allProjects}>
-              <div className={useinfo.title4}>
-                <h2>All projects worked By :- {user.name}</h2>
-                {/* <div className={useinfo.managername}>
+              <div id="Projects" className={useinfo.allProjects}>
+                <div className={useinfo.title4}>
+                  <h2>All projects worked By :- {user.name}</h2>
+                  {/* <div className={useinfo.managername}>
                   <h3>Managed by : Rahulkumar</h3>
                 </div> */}
-              </div>
+                </div>
 
-              <div className={useinfo.fiterprouwise}>
-                {/* <div className={useinfo.filbtn}>
+                <div className={useinfo.fiterprouwise}>
+                  {/* <div className={useinfo.filbtn}>
                   <button>Runing</button>
                   <button>Completed</button>
                   <button>Pending</button>
                 </div> */}
-              </div>
-              <div className={useinfo.projectbox}>
-                {userProjects.length ? (
-                  Object.values(userProjects).map((data) => (
-                    <div className={useinfo.projectwise}>
-                      <div className={useinfo.protitle}>
-                        <h3>{data.projectTitle}</h3>
+                </div>
+                <div className={useinfo.projectbox}>
+                  {userProjects.length ? (
+                    Object.values(userProjects).map((data) => (
+                      <div key={uuidv4()} className={useinfo.projectwise}>
+                        <div className={useinfo.protitle}>
+                          <h3>{data.projectTitle}</h3>
+                        </div>
+                        <h3>{data.clientName}</h3>
+                        <h4>{data.timeLine}</h4>
                       </div>
-                      <h3>{data.clientName}</h3>
-                      <h4>{data.timeLine}</h4>
-                    </div>
-                  ))
-                ) : (
-                  <h1>Not any Projects for display </h1>
-                )}
+                    ))
+                  ) : (
+                    <h1>Not any Projects for display </h1>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
